ab4a87ba2909c01165858f048806f9e0fbc8d8cb
kim <honkang@163.com>
2015-04-28 11:43:56 +0800

    u

--- memcacheDemo/src/com/qcloud/memcache/test/SpyMemcachedManager.java
+++ memcacheDemo/src/com/qcloud/memcache/test/SpyMemcachedManager.java
@@ -18,28 +18,23 @@ import java.util.concurrent.TimeUnit;
 public class SpyMemcachedManager {
 
     public static void main(String[] args){
-        String testTarget = (args.length > 0 && !args[0].isEmpty()) ? args[0] : "";
         String testMethod = (args.length > 1 && !args[1].isEmpty()) ? args[1] : "";
         String testUrl = (args.length > 2 && !args[2].isEmpty()) ? args[2] : "";
         int count = (args.length > 3 && !args[3].isEmpty()) ? Integer.parseInt(args[3].toString()) : 10;
         String connectionType = (args.length > 4 && !args[4].isEmpty()) ? args[4] : "";
 
-        System.out.println("start - " + (testTarget != null ? testTarget : "none args"));
+        System.out.println("start test - method = " + testMethod + ", testUrl = " + testUrl + ", testCount = " + count);
 
-        SpyMemcachedManager manager = new SpyMemcachedManager();
-        if(testTarget.equals("ali")) {
-            manager.testAli(args.length > 1 ? args[1] : null);
-        }else{
+        if(testUrl.length() >= 0) {
+            SpyMemcachedManager manager = new SpyMemcachedManager();
             manager.testQcloud(testMethod, testUrl, count, connectionType);
+        }else{
+            System.out.println("缺少url");
         }
     }
 
     public void testQcloud(String type, String url, int count, String connectionType){
         log("info", "start test qcloud");
-        final String host = "10.66.108.24";
-        final String port = "9101";
-        url = url.length() > 0 ? url : host + ":" + port;
-
         MemcachedClient cache = null;
         try {
             DefaultConnectionFactory f;
@@ -53,19 +48,10 @@ public class SpyMemcachedManager {
 
             log("info", "connected");
 
-            //向OCS中存一个key为"ocs"的数据，便于后面验证读取数据
-            OperationFuture future = cache.set("ocs", 1000, " Open Cache Service,  from www.qcloud.com");
-
-            //执行get操作，从缓存中读数据,读取key为"ocs"的数据
-            log("get操作", cache.get("ocs").toString());
-
-            future.get(); //  确保之前(mc.set())操作已经结束
-            log("info", "future get finished");
-
             if(type.equals("whileSet")){
-                whileSet(cache);
+                whileSet(cache, count);
             }else if(type.equals("whileGet")){
-                whileGet(cache);
+                whileGet(cache, count);
             }else if(type.equals("normal")){
                 testNormal(cache, count);
             }else if(type.equals("concurrency")){
@@ -73,17 +59,24 @@ public class SpyMemcachedManager {
             }else if(type.equals("asyncGet")){
                 testAsyncGet(cache, count);
             }
-
-
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
             e.printStackTrace();
         }
         if (cache != null) {
             cache.shutdown();
+        }
+    }
+
+    public void onlySet(MemcachedClient cache, int count){
+        log("info", "-----------------start test only set----------------");
+        for (int i = 0; i < count; i++) {
+            long t = new Date().getTime();
+            String key = "spymemcache-only-set-key-" + i;
+            String value = "spymemcache-only-set-vlaue-" + i + "-" + t;
+
+            //执行set操作，向缓存中存数据
+            cache.set(key, 1000, value);
+            log("set操作", key + "=" + value);
         }
     }
 
@@ -173,12 +166,11 @@ public class SpyMemcachedManager {
 
     /**
      * 循环写一个key
-     * @param tag
-     * @param msg
      */
-    public void whileSet(MemcachedClient cache){
+    public void whileSet(MemcachedClient cache, int count){
         log("info", "-----------------start test while set----------------");
-        while(true){
+        int i = 0;
+        while(i++ < count){
             String key = "spymemcache-whileSet-key";
             String value = "spymemcache-whileSet-value-" + new Date().getTime();
             cache.set(key, 1000, value);
@@ -188,13 +180,11 @@ public class SpyMemcachedManager {
 
     /**
      * 循环读一个key
-     * @param tag
-     * @param msg
      */
-
-    public void whileGet(MemcachedClient cache){
+    public void whileGet(MemcachedClient cache, int count){
         log("info", "-----------------start test while get----------------");
-        while(true){
+        int i = 0;
+        while(i++ < count){
             String key = "spymemcache-whileSet-key";
             String value = cache.get(key).toString();
             log("while getted", key + "=" + value);
@@ -202,57 +192,5 @@ public class SpyMemcachedManager {
     }
     public void log(String tag, String msg){
         System.out.println("["+new Date().getTime()+"] " + "[" + tag + "] " + msg);
-    }
-
-    public void testAli(String pwd){
-        System.out.println("start test aliyun");
-        final String host = "834103b87c6111e4.m.cnqdalicm9pub001.ocs.aliyuncs.com";
-        final String port = "11211";
-        final String uname = "834103b87c6111e4";
-
-        if(pwd.isEmpty()){
-            System.out.println("pwd is null");
-            return;
-        }
-
-        MemcachedClient memcachedClient = null;
-        try {
-            AuthDescriptor authDescriptor = new AuthDescriptor(new String[]{"PLAIN"}, new PlainCallbackHandler(uname, pwd));
-            ConnectionFactory connectionFactory = new ConnectionFactoryBuilder().setProtocol(ConnectionFactoryBuilder.Protocol.BINARY).setAuthDescriptor(authDescriptor).build();
-            List<InetSocketAddress> list = AddrUtil.getAddresses(host + ":" + port);
-            memcachedClient = new MemcachedClient(connectionFactory, list);
-
-            System.out.println("OCS Sample Code");
-
-            //向OCS中存一个key为"ocs"的数据，便于后面验证读取数据
-            OperationFuture future = memcachedClient.set("ocs", 1000," Open Cache Service,  from www.Aliyun.com");
-
-            //向OCS中存若干个数据，随后可以在OCS控制台监控上看到统计信息
-            for(int i=0;i<10;i++){
-                String key="key-"+i;
-                String value="value-"+i + "-" + new Date();
-
-                //执行set操作，向缓存中存数据
-                memcachedClient.set(key, 1000, value);
-            }
-
-
-            System.out.println("Set操作完成!");
-
-            future.get();  //  确保之前(mc.set())操作已经结束
-
-            //执行get操作，从缓存中读数据,读取key为"ocs"的数据
-            System.out.println("Get操作:"+memcachedClient.get("ocs"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        if (memcachedClient != null) {
-            memcachedClient.shutdown();
-        }
     }
 }
